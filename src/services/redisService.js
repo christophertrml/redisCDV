42efbad7d9d2c08d8355c36a0efd5b6b53b0db91
Chris Tramel <hello@christophertrml.com>
2019-02-25 01:48:15 -0500

    Start work on getKeys

--- src/services/redisService.js
+++ src/services/redisService.js
@@ -1,5 +1,25 @@
+import Redis from 'ioredis';
 export default class RedisService {
     async getKeys(keyFilter, redisConnection) {
-        return [];
+        console.log(redisConnection.redisServerUris);
+        const cluster = new Redis.Cluster(redisConnection.redisServerUris);
+        const nodes = cluster.nodes();
+        console.log(nodes);
+        return await Promise.all(nodes.map(node => {
+            let keyMatches = [];
+            console.log(node);
+            console.log(keyFilter);
+            const stream = node.scanStream({
+                match: keyFilter.query
+            });
+            stream.on('data', resultKeys => {
+                keyMatches = keyMatches.concat(resultKeys);
+                console.log(resultKeys);
+            })
+            stream.on('end', () => {
+                return Promise.resolve(keyMatches);
+            })
+        }));
+
     }
 }
--- test/integration/services/redisService.test.js
+++ test/integration/services/redisService.test.js
@@ -1,19 +1,27 @@
 import RedisService from '../../../src/services/redisService';
 import RedisConnection from '../../../src/models/redisConnection';
-import KeyFilter from '../models/keyFilter';
+import KeyFilter from '../../../src/models/keyFilter';
 import Redis from 'ioredis';
 
 test('RedisService can get all keys from redis', async done => {
     const redisUri = process.env['TEST_REDIS_URI_1'];
-    const redis = new Redis()
-    redis.set('foo', 'bar');
-    redis.get('foo', function (err, result) {
-        console.log(result);
-        done();
-    });
+    const redis = new Redis();
+    // Create 20 keys that match and 80 that don't
+    let keysToFind = [];
+    
+    for (let i = 0; i < 20; i++) {
+        redis.set('fo_' + i, true);
+        keysToFind.push('fo_' + i);
+
+        redis.set('fa_' + i, true);
+        redis.set('fi_' + i, true);
+        redis.set('fp_' + i, true);
+        redis.set('fu_' + i, true);
+    }
     
     const service = new RedisService();
     const connection = new RedisConnection("test", [redisUri]);
-    const results = service.getKeys(new KeyFilter("fo*"), connection);
-    expect(results).toEqual([]);
+    const results = await service.getKeys(new KeyFilter("fo_*"), connection);
+    expect(results).toEqual(keysToFind);
+    done();
 });
